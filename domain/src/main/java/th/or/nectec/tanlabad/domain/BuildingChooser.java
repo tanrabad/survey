dc165bfb1ccc421031216d392236c699540461f7
Piruin Panichphol <piruin.p@gmail.com>
2015-11-01 01:13:22 +0700

    move BuildingChooser to production

---
+++ domain/src/main/java/th/or/nectec/tanlabad/domain/BuildingChooser.java
@@ -0,0 +1,30 @@
+package th.or.nectec.tanlabad.domain;
+
+import th.or.nectec.tanrabad.Building;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class BuildingChooser {
+    private final BuildingRepository buildingRepository;
+    private final BuildingPresenter presenter;
+
+    public BuildingChooser(BuildingRepository buildingRepository, BuildingPresenter presenter) {
+
+        this.buildingRepository = buildingRepository;
+        this.presenter = presenter;
+    }
+
+    public void showBuildingOf(UUID placeUuid) {
+        if (placeUuid == null) {
+            presenter.showPleaseSpecityPlace();
+            return;
+        }
+
+        ArrayList<Building> buildingInPlace = buildingRepository.findBuildingInPlace(placeUuid);
+        if (buildingInPlace != null)
+            presenter.showBuildingList(buildingInPlace);
+        else
+            presenter.showNotFoundBuilding();
+    }
+}
--- domain/src/test/java/th/or/nectec/tanlabad/domain/BuildingChooserTest.java
+++ domain/src/test/java/th/or/nectec/tanlabad/domain/BuildingChooserTest.java
@@ -76,27 +76,4 @@ public class BuildingChooserTest {
     }
 
 
-    private class BuildingChooser {
-        private final BuildingRepository buildingRepository;
-        private final BuildingPresenter presenter;
-
-        public BuildingChooser(BuildingRepository buildingRepository, BuildingPresenter presenter) {
-
-            this.buildingRepository = buildingRepository;
-            this.presenter = presenter;
-        }
-
-        public void showBuildingOf(UUID placeUuid) {
-            if (placeUuid == null) {
-                presenter.showPleaseSpecityPlace();
-                return;
-            }
-
-            ArrayList<Building> buildingInPlace = buildingRepository.findBuildingInPlace(placeUuid);
-            if (buildingInPlace != null)
-                presenter.showBuildingList(buildingInPlace);
-            else
-                presenter.showNotFoundBuilding();
-        }
-    }
 }

0aae6837c0324dd68e8f284429fe5e528606bb31
icesuxx <ladawan_k@hotmail.co.th>
2015-11-10 15:01:33 +0700

    เพิ่ม Stub ของ Building ที่ผูกกับ Stub ของ Place

--- app/src/main/java/th/or/nectec/tanrabad/survey/repository/StubBuildingRepository.java
+++ app/src/main/java/th/or/nectec/tanrabad/survey/repository/StubBuildingRepository.java
@@ -19,6 +19,7 @@ package th.or.nectec.tanrabad.survey.repository;
 
 import th.or.nectec.tanrabad.domain.BuildingRepository;
 import th.or.nectec.tanrabad.entity.Building;
+import th.or.nectec.tanrabad.entity.Place;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,9 +31,43 @@ public class StubBuildingRepository implements BuildingRepository {
     @Override
     public List<Building> findBuildingInPlace(UUID placeUuid) {
         List<Building> buildings = new ArrayList<>();
-        buildings.add(Building.withName("214/43"));
-        buildings.add(Building.withName("214/44"));
-        buildings.add(Building.withName("214/50"));
+
+        Building building1 = (new Building(UUID.nameUUIDFromBytes("1xyz".getBytes()), "214/43"));
+        building1.setPlace(new Place(UUID.nameUUIDFromBytes("1abc".getBytes()), "บางไผ่"));
+        buildings.add(building1);
+
+        Building building2 = (new Building(UUID.nameUUIDFromBytes("2xyz".getBytes()), "214/44"));
+        building2.setPlace(new Place(UUID.nameUUIDFromBytes("1abc".getBytes()), "บางไผ่"));
+        buildings.add(building2);
+
+        Building building3 = (new Building(UUID.nameUUIDFromBytes("3xyz".getBytes()), "214/45"));
+        building3.setPlace(new Place(UUID.nameUUIDFromBytes("1abc".getBytes()), "บางไผ่"));
+        buildings.add(building3);
+
+        Building building4 = (new Building(UUID.nameUUIDFromBytes("1opj".getBytes()), "45/1"));
+        building4.setPlace(new Place(UUID.nameUUIDFromBytes("1bcd".getBytes()), "บางโพธิ์"));
+        buildings.add(building4);
+
+        Building building5 = (new Building(UUID.nameUUIDFromBytes("2opj".getBytes()), "45/2"));
+        building5.setPlace(new Place(UUID.nameUUIDFromBytes("1bcd".getBytes()), "บางโพธิ์"));
+        buildings.add(building5);
+
+        Building building6 = (new Building(UUID.nameUUIDFromBytes("3opj".getBytes()), "45/3"));
+        building6.setPlace(new Place(UUID.nameUUIDFromBytes("1bcd".getBytes()), "บางโพธิ์"));
+        buildings.add(building6);
+
+        Building building7 = (new Building(UUID.nameUUIDFromBytes("1hij".getBytes()), "4/1"));
+        building7.setPlace(new Place(UUID.nameUUIDFromBytes("3def".getBytes()), "บางไทร"));
+        buildings.add(building7);
+
+        Building building8 = (new Building(UUID.nameUUIDFromBytes("2hij".getBytes()), "4/2"));
+        building8.setPlace(new Place(UUID.nameUUIDFromBytes("3def".getBytes()),"บางไทร" ));
+        buildings.add(building8);
+
+        Building building9 = (new Building(UUID.nameUUIDFromBytes("3hij".getBytes()), "4/3"));
+        building9.setPlace(new Place(UUID.nameUUIDFromBytes("3def".getBytes()), "บางไทร"));
+        buildings.add(building9);
+
         return buildings;
     }
 

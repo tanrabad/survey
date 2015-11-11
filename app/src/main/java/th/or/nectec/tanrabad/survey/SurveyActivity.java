08054a4ee33aa1b7419bfd51023876134d3f7079
N. Choatravee <ch.n.17699@gmail.com>
2015-11-11 15:58:28 +0700

    เริ่มทำการ implement usecase ของ SurveyActivity

--- app/src/main/java/th/or/nectec/tanrabad/survey/SurveyActivity.java
+++ app/src/main/java/th/or/nectec/tanrabad/survey/SurveyActivity.java
@@ -20,41 +20,120 @@ package th.or.nectec.tanrabad.survey;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
+import java.util.UUID;
 
 import th.or.nectec.tanrabad.domain.ContainerController;
 import th.or.nectec.tanrabad.domain.ContainerPresenter;
+import th.or.nectec.tanrabad.domain.SurveyController;
+import th.or.nectec.tanrabad.domain.SurveyPresenter;
+import th.or.nectec.tanrabad.domain.SurveyRepository;
+import th.or.nectec.tanrabad.domain.UserRepository;
+import th.or.nectec.tanrabad.entity.Building;
 import th.or.nectec.tanrabad.entity.ContainerType;
+import th.or.nectec.tanrabad.entity.Survey;
+import th.or.nectec.tanrabad.entity.User;
 import th.or.nectec.tanrabad.survey.repository.InMemoryContainerTypeRepository;
+import th.or.nectec.tanrabad.survey.repository.StubBuildingRepository;
+import th.or.nectec.tanrabad.survey.repository.StubPlaceRepository;
 
 public class SurveyActivity extends AppCompatActivity {
+
+    Building surveyBuilding;
+    User surveyUser;
+
+    SurveyController surveyController;
+    ContainerController containerController;
+
+    TextView containerList;
+    ContainerPresenter containerPresenter = new ContainerPresenter() {
+        @Override
+        public void showContainerList(List<ContainerType> containers) {
+            String containerListStr = "";
+            for (ContainerType eachContainer : containers) {
+                containerListStr += eachContainer.toString() + "\n";
+            }
+            containerList.setText(containerListStr);
+        }
+
+        @Override
+        public void showContainerNotFound() {
+            containerList.setText("ไม่เจอรายการภาชนะ");
+        }
+    };
+    SurveyPresenter surveyPresenter = new SurveyPresenter() {
+        @Override
+        public void onEditSurvey(Survey survey) {
+            Toast.makeText(SurveyActivity.this, "แก้ไขสำรวจ", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onNewSurvey(Building building, User user) {
+            surveyBuilding = building;
+            surveyUser = user;
+            Toast.makeText(SurveyActivity.this, "สำรวจใหม่", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void alertUserNotFound() {
+            Toast.makeText(SurveyActivity.this, "ไม่พบข้อมูลผู้ใช้งาน", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void alertBuildingNotFound() {
+            Toast.makeText(SurveyActivity.this, "ไม่พบข้อมูลอาคาร", Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
 
-        final TextView containerList = (TextView) findViewById(R.id.container_list);
+        containerList = (TextView) findViewById(R.id.container_list);
 
-        ContainerController containerController = new ContainerController(InMemoryContainerTypeRepository.getInstance(), new ContainerPresenter() {
-            @Override
-            public void showContainerList(List<ContainerType> containers) {
-                String containerListStr = "";
-                for (ContainerType eachContainer : containers) {
-                    containerListStr += eachContainer.toString() + "\n";
-                }
-                containerList.setText(containerListStr);
-            }
+        initSurvey();
+    }
 
-            @Override
-            public void showContainerNotFound() {
-                containerList.setText("ไม่เจอรายการภาชนะ");
-            }
-        });
-
+    private void initSurvey() {
+        initController();
         containerController.showList();
+        surveyController.checkThisBuildingAndUserCanSurvey(UUID.nameUUIDFromBytes("2xyz".getBytes()).toString(), "sara");
+    }
 
+    private void initController() {
+
+        UserRepository userRepository = new UserRepository() {
+            @Override
+            public User findUserByName(String userName) {
+                User user = User.fromUsername("sara");
+                if (user.getUsername().equals(userName)) {
+                    return user;
+                }
+                return null;
+            }
+        };
+
+        SurveyRepository surveyRepository = new SurveyRepository() {
+            @Override
+            public boolean save(Survey survey) {
+                return false;
+            }
+
+            @Override
+            public Survey findByBuildingAndUserIn7Day(Building building, User user) {
+                StubPlaceRepository stubPlaceRepository = new StubPlaceRepository();
+                Building building2 = (new Building(UUID.nameUUIDFromBytes("2xyz".getBytes()), "214/44"));
+                building2.setPlace(stubPlaceRepository.getPalazzettoVillage());
+                return null;
+            }
+
+        };
+
+        containerController = new ContainerController(InMemoryContainerTypeRepository.getInstance(), containerPresenter);
+        surveyController = new SurveyController(surveyRepository, new StubBuildingRepository(), userRepository, surveyPresenter);
     }
 
 }

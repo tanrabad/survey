d85abaed68c64f054628593a53c2a9d539d04989
Puy <porntipa.chok@gmail.com>
2015-11-11 16:12:54 +0700

    สร้าง stub repository ของ User

---
+++ app/src/main/java/th/or/nectec/tanrabad/survey/repository/StubUserRepository.java
@@ -0,0 +1,29 @@
+package th.or.nectec.tanrabad.survey.repository;
+
+
+import th.or.nectec.tanrabad.domain.UserRepository;
+import th.or.nectec.tanrabad.entity.User;
+
+public class StubUserRepository implements UserRepository {
+
+    private final User sara;
+
+    public StubUserRepository() {
+        StubUserRepository stubUserRepository = new StubUserRepository();
+        sara = new User("sara");
+        sara.setFirstname("ซาร่า");
+        sara.setLastname("คิดส์");
+        sara.setEmail("sara.k@gmail.com");
+        sara.setOrganizationId(1);
+    }
+
+    @Override
+    public User findUserByName(String userName) {
+        if (userName.equals(userName)) {
+            return sara ;
+        } else {
+            return null;
+        }
+    }
+
+}

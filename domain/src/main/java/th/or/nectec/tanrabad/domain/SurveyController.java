55422da3983f1b8daeab6d5849e48da6506f8dd4
N. Choatravee <ch.n.17699@gmail.com>
2015-11-11 15:52:46 +0700

    ปรับปรุงชุด Test ของ LoadSurveyTest จากเดิมที่ต้องใช้ object ของ building และ user โดยตรง เปลี่ยนมาเป็นให้เรียกจาก String ของ username และ buildingUUID ในการหา object user กับ building

--- domain/src/main/java/th/or/nectec/tanrabad/domain/SurveyController.java
+++ domain/src/main/java/th/or/nectec/tanrabad/domain/SurveyController.java
@@ -1,29 +1,31 @@
 package th.or.nectec.tanrabad.domain;
 
+import java.util.UUID;
+
 import th.or.nectec.tanrabad.entity.Building;
 import th.or.nectec.tanrabad.entity.Survey;
 import th.or.nectec.tanrabad.entity.User;
 
-class SurveyController {
+public class SurveyController {
     private SurveyRepository surveyRepository;
     private SurveyPresenter surveyPresenter;
 
-    public SurveyController(SurveyRepository surveyRepository, SurveyPresenter surveyPresenter) {
+    private BuildingRepository buildingRepository;
+    private UserRepository userRepository;
+
+    public SurveyController(SurveyRepository surveyRepository, BuildingRepository buildingRepository, UserRepository userRepository, SurveyPresenter surveyPresenter) {
         this.surveyRepository = surveyRepository;
+        this.buildingRepository = buildingRepository;
+        this.userRepository = userRepository;
         this.surveyPresenter = surveyPresenter;
     }
 
-    public void checkThisBuildingAndUserCanSurvey(Building building, User user) {
+    public void checkThisBuildingAndUserCanSurvey(String buildingUUID, String username) {
+        User user = checkUserExist(username);
+        if (user == null) return;
 
-        if (building == null) {
-            surveyPresenter.alertBuildingNotFound();
-            return;
-        }
-
-        if (user == null) {
-            surveyPresenter.alertUserNotFound();
-            return;
-        }
+        Building building = checkBuildingExist(buildingUUID);
+        if (building == null) return;
 
         Survey survey = surveyRepository.findByBuildingAndUserIn7Day(building, user);
         if (survey == null) {
@@ -31,5 +33,23 @@ class SurveyController {
         } else {
             surveyPresenter.onEditSurvey(survey);
         }
+    }
+
+    private User checkUserExist(String username) {
+        User user = userRepository.findUserByName(username);
+        if (user == null) {
+            surveyPresenter.alertUserNotFound();
+            return null;
+        }
+        return user;
+    }
+
+    private Building checkBuildingExist(String buildingUUID) {
+        Building building = buildingRepository.findBuildingByUUID(UUID.fromString(buildingUUID));
+        if (building == null) {
+            surveyPresenter.alertBuildingNotFound();
+            return null;
+        }
+        return building;
     }
 }

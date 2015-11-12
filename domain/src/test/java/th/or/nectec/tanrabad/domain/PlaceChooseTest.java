73a7a8a7ba61f8dc0a17167116cc49f389f7278e
N. Choatravee <ch.n.17699@gmail.com>
2015-11-12 17:13:35 +0700

    เพิ่ม Test Case ในกรณีที่ไม่พบรายการ Place

--- domain/src/test/java/th/or/nectec/tanrabad/domain/PlaceChooseTest.java
+++ domain/src/test/java/th/or/nectec/tanrabad/domain/PlaceChooseTest.java
@@ -2,6 +2,7 @@ package th.or.nectec.tanrabad.domain;
 
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -14,12 +15,17 @@ public class PlaceChooseTest {
 
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
+    private PlaceRepository placeRepository;
+    private PlaceListPresenter placeListPresenter;
+
+    @Before
+    public void setUp() {
+        placeRepository = context.mock(PlaceRepository.class);
+        placeListPresenter = context.mock(PlaceListPresenter.class);
+    }
 
     @Test
     public void getPlaceList() {
-        final PlaceRepository placeRepository = context.mock(PlaceRepository.class);
-        final PlaceListPresenter placeListPresenter = context.mock(PlaceListPresenter.class);
-
         final List<Place> places = new ArrayList<>();
         places.add(Place.withName("Vaillage A"));
         places.add(Place.withName("Vaillage C"));
@@ -35,4 +41,16 @@ public class PlaceChooseTest {
         chooser.getPlaceList();
     }
 
+    @Test
+    public void testPlaceListNotFound() throws Exception {
+        context.checking(new Expectations() {
+            {
+                oneOf(placeRepository).findPlaces();
+                will(returnValue(null));
+                oneOf(placeListPresenter).displayPlaceNotFound();
+            }
+        });
+        PlaceChooser chooser = new PlaceChooser(placeRepository, placeListPresenter);
+        chooser.getPlaceList();
+    }
 }

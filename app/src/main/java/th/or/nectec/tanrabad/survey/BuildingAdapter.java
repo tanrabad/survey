fd320e33291a097310e9926045e613f4b47d4aef
icesuxx <ladawan_k@hotmail.co.th>
2015-11-12 14:58:49 +0700

    สร้าง Adapter เพื่อแสดงรายการ Building

---
+++ app/src/main/java/th/or/nectec/tanrabad/survey/BuildingAdapter.java
@@ -0,0 +1,73 @@
+package th.or.nectec.tanrabad.survey;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
+import th.or.nectec.tanrabad.entity.Building;
+
+public class BuildingAdapter extends BaseAdapter {
+
+    Context context;
+
+
+    ArrayList<Building> buildings = new ArrayList<>();
+
+    public BuildingAdapter(Context context, List<Building> buildings) {
+        this.context = context;
+        this.buildings.addAll(buildings);
+    }
+
+    @Override
+    public int getCount() {
+        return buildings.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return buildings.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        final ViewHolder holder;
+        if(view == null || view.getTag() == null){
+            view = inflater.inflate(R.layout.building_list_item, parent, false);
+            holder = new ViewHolder();
+            holder.buildingTextView = (TextView)view.findViewById(R.id.building_name);
+            holder.buildingIcon = (ImageView)view.findViewById(R.id.building_icon);
+
+            view.setTag(holder);
+        }else{
+            holder = (ViewHolder) view.getTag();
+        }
+        holder.buildingTextView.setText(buildings.get(i).getName());
+        view.setTag(holder);
+
+        return view;
+    }
+
+    public  class ViewHolder {
+        TextView buildingTextView;
+        ImageView buildingIcon;
+    }
+}
+
+
+
+

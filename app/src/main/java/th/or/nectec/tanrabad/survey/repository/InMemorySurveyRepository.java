420e8f5de05e09f3d4a5a010132c41982e85e814
N. Choatravee <ch.n.17699@gmail.com>
2015-11-12 11:34:33 +0700

    ทำการ implement InMemorySurveyRepository

--- app/src/main/java/th/or/nectec/tanrabad/survey/repository/InMemorySurveyRepository.java
+++ app/src/main/java/th/or/nectec/tanrabad/survey/repository/InMemorySurveyRepository.java
@@ -1,7 +1,43 @@
 package th.or.nectec.tanrabad.survey.repository;
 
+import java.util.ArrayList;
+
+import th.or.nectec.tanrabad.domain.SurveyRepository;
+import th.or.nectec.tanrabad.entity.Building;
+import th.or.nectec.tanrabad.entity.Survey;
+import th.or.nectec.tanrabad.entity.User;
+
 /**
  * Created by N. Choatravee on 12/11/2558.
  */
-public class InMemorySurveyRepository {
+public class InMemorySurveyRepository implements SurveyRepository {
+
+    private static InMemorySurveyRepository instance;
+    ArrayList<Survey> surveys;
+
+    public InMemorySurveyRepository() {
+        this.surveys = new ArrayList<>();
+    }
+
+    public static InMemorySurveyRepository getInstance() {
+        if (instance == null)
+            instance = new InMemorySurveyRepository();
+        return instance;
+    }
+
+    @Override
+    public boolean save(Survey survey) {
+        surveys.add(survey);
+        return false;
+    }
+
+    @Override
+    public Survey findByBuildingAndUserIn7Day(Building building, User user) {
+        for (Survey eachSurvey : surveys) {
+            if (eachSurvey.getSurveyBuilding().equals(building) && eachSurvey.getUser().equals(user)) {
+                return eachSurvey;
+            }
+        }
+        return null;
+    }
 }

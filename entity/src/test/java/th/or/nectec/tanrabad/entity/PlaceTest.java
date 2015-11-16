bd7fb882816ebc5206b33a547988e599b1327bb6
Puy <porntipa.chok@gmail.com>
2015-11-16 11:36:30 +0700

    refactor test สำหรับ entity Place

--- entity/src/test/java/th/or/nectec/tanrabad/entity/PlaceTest.java
+++ entity/src/test/java/th/or/nectec/tanrabad/entity/PlaceTest.java
@@ -15,62 +15,55 @@ public class PlaceTest {
     public static final UUID BANGPHAI_UUID = UUID.randomUUID();
     public static final String BANGPHAI_NAME = "บางไผ่";
     public static final int BANGPHAI_TYPE = Place.TYPE_VILLAGE_COMMUNITY;
+    private final Place place1 = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
+    private final Place place2 = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
 
     @Test
-    public void testPlaceWithName() throws Exception {
+    public void testWithName() throws Exception {
         Place place = Place.withName(BANGPHAI_NAME);
         assertEquals(BANGPHAI_NAME, place.getName());
     }
 
     @Test
-    public void testGetPlaceId() throws Exception {
-        Place place = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
-        assertEquals(BANGPHAI_UUID, place.getId());
+    public void testGetId() throws Exception {
+        assertEquals(BANGPHAI_UUID, place1.getId());
     }
 
     @Test
-    public void testGetPlaceName() throws Exception {
-        Place place = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
-        assertEquals(BANGPHAI_NAME, place.getName());
+    public void testGetName() throws Exception {
+        assertEquals(BANGPHAI_NAME, place1.getName());
     }
 
     @Test
     public void testSetThenGetPlaceName() throws Exception {
-        Place place = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
-        place.setName("บางโพธิ์");
-        assertEquals("บางโพธิ์", place.getName());
+        place1.setName("บางโพธิ์");
+        assertEquals("บางโพธิ์", place1.getName());
     }
 
     @Test
     public void testSetThenGetPlaceType() throws Exception {
-        Place place = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
-        place.setType(Place.TYPE_SCHOOL);
-        assertEquals(Place.TYPE_SCHOOL, place.getType());
+        place1.setType(Place.TYPE_SCHOOL);
+        assertEquals(Place.TYPE_SCHOOL, place1.getType());
     }
 
     @Test
     public void placeWithDifferentNameMustNotEqual() throws Exception {
-        Place place1 = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
         place1.setType(BANGPHAI_TYPE);
-        Place place2 = new Place(BANGPHAI_UUID, "บางโพธิ์");
-        place2.setType(BANGPHAI_TYPE);
+        place2.setType(place1.getType());
+        place2.setName("บางโพธิ์");
         assertNotEquals(place1, place2);
     }
 
     @Test
     public void placeWithDifferentTypeMustNotEqual() throws Exception {
-        Place place1 = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
         place1.setType(Place.TYPE_FACTORY);
-        Place place2 = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
         place2.setType(Place.TYPE_SCHOOL);
         assertNotEquals(place1, place2);
     }
 
     @Test
     public void placeWithTheSameNameAndTypeMustEqual() throws Exception {
-        Place place1 = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
         place1.setType(BANGPHAI_TYPE);
-        Place place2 = new Place(BANGPHAI_UUID, BANGPHAI_NAME);
         place2.setType(BANGPHAI_TYPE);
         assertEquals(place1, place2);
     }

303f609758dc773311a02877caf6e2abaa926758
N. Choatravee <ch.n.17699@gmail.com>
2015-11-17 11:40:53 +0700

    ปรับปรุง PlaceAdapter ให้สามารถ update และล้างรายการ ใน adapter ได้

--- app/src/main/java/th/or/nectec/tanrabad/survey/PlaceAdapter.java
+++ app/src/main/java/th/or/nectec/tanrabad/survey/PlaceAdapter.java
@@ -24,10 +24,11 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import th.or.nectec.tanrabad.entity.Place;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import th.or.nectec.tanrabad.entity.Place;
 
 public class PlaceAdapter extends BaseAdapter {
 
@@ -35,9 +36,24 @@ public class PlaceAdapter extends BaseAdapter {
 
     ArrayList<Place> places = new ArrayList<>();
 
+    public PlaceAdapter(Context context) {
+        this.context = context;
+    }
+
     public PlaceAdapter(Context context, List<Place> places) {
         this.context = context;
         this.places.addAll(places);
+    }
+
+    public void updateData(List<Place> places) {
+        this.places.clear();
+        this.places.addAll(places);
+        notifyDataSetChanged();
+    }
+
+    public void clearData() {
+        this.places.clear();
+        notifyDataSetChanged();
     }
 
     @Override
@@ -57,17 +73,17 @@ public class PlaceAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         final ViewHolder holder;
-        if(view == null || view.getTag() == null){
+        if (view == null || view.getTag() == null) {
             view = inflater.inflate(R.layout.place_list_item, parent, false);
             holder = new ViewHolder();
-            holder.placeTextView = (TextView)view.findViewById(R.id.place_name);
-            holder.placeIcon = (ImageView)view.findViewById(R.id.place_icon);
+            holder.placeTextView = (TextView) view.findViewById(R.id.place_name);
+            holder.placeIcon = (ImageView) view.findViewById(R.id.place_icon);
 
             view.setTag(holder);
-        }else{
+        } else {
             holder = (ViewHolder) view.getTag();
         }
         holder.placeTextView.setText(places.get(i).getName());
@@ -75,7 +91,8 @@ public class PlaceAdapter extends BaseAdapter {
 
         return view;
     }
-    public  class ViewHolder {
+
+    public class ViewHolder {
         TextView placeTextView;
         ImageView placeIcon;
     }

159e62f71a6d5d4606d632db00865690c12c7246
N. Choatravee <ch.n.17699@gmail.com>
2015-11-17 11:37:13 +0700

    เพิ่ม dropdown สำหรับการแสดงรายการ place ทั้งหมด

--- app/src/main/java/th/or/nectec/tanrabad/survey/PlaceTypeAdapter.java
+++ app/src/main/java/th/or/nectec/tanrabad/survey/PlaceTypeAdapter.java
@@ -21,6 +21,7 @@ public class PlaceTypeAdapter extends BaseAdapter {
         this.context = context;
 
         ArrayList<PlaceType> placeTypes = new ArrayList<>();
+        placeTypes.add(new PlaceType(-1, "ทั้งหมด"));
         placeTypes.add(new PlaceType(Place.TYPE_VILLAGE_COMMUNITY, "ชุมชน"));
         placeTypes.add(new PlaceType(Place.TYPE_WORSHIP, "ศาสนสถาน"));
         placeTypes.add(new PlaceType(Place.TYPE_SCHOOL, "โรงเรียน"));

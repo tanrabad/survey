82e6f0853816317285b5bd6060ae7b8630abea64
icesuxx <ladawan_k@hotmail.co.th>
2015-11-24 17:19:33 +0700

    refactor FilterBoundaryCalculate

--- domain/src/main/java/th/or/nectec/tanrabad/domain/FilterBoundaryCalculate.java
+++ domain/src/main/java/th/or/nectec/tanrabad/domain/FilterBoundaryCalculate.java
@@ -4,33 +4,23 @@ import th.or.nectec.tanrabad.entity.Location;
 
 public class FilterBoundaryCalculate implements FilterBoundaryCalculator {
 
-    double currentLatitude = 40.6892;
-    double currentLongitude = -74.0444;
-    final Location currentLocation = new Location(currentLatitude, currentLongitude);
-
-    private double minimumLongitude;
-    private double minimumLatitude;
-    private double maximumLongitude;
-    private double maximumLatitude;
-
-    double distanceInKm = 100;
-
-    private double longitudeDegreeFromKm  = distanceInKm/111.699;
-    private double latitudeDegreeFromKm = distanceInKm/110.567;
-
     @Override
     public Location getMinLocation(Location currentLocation, int distanceInKm) {
-        minimumLongitude = currentLocation.getLongitude() - longitudeDegreeFromKm;
-        minimumLatitude = currentLocation.getLatitude() - latitudeDegreeFromKm;
+        double longitudeDegreeFromKm  = distanceInKm/111.699;
+        double latitudeDegreeFromKm = distanceInKm/110.567;
+        double minimumLongitude = currentLocation.getLongitude() - longitudeDegreeFromKm;
+        double minimumLatitude = currentLocation.getLatitude() - latitudeDegreeFromKm;
 
-        return  new Location(minimumLatitude, minimumLongitude);
+        return new Location(minimumLatitude, minimumLongitude);
     }
 
     @Override
     public Location getMaxLocation(Location currentLocation, int distanceInKm) {
-        maximumLongitude = currentLocation.getLongitude() + longitudeDegreeFromKm;
-        maximumLatitude = currentLocation.getLatitude() + latitudeDegreeFromKm;
+        double longitudeDegreeFromKm  = distanceInKm/111.699;
+        double latitudeDegreeFromKm = distanceInKm/110.567;
+        double maximumLongitude = currentLocation.getLongitude() + longitudeDegreeFromKm;
+        double maximumLatitude = currentLocation.getLatitude() + latitudeDegreeFromKm;
 
-        return  new Location(maximumLatitude, maximumLongitude);
+        return new Location(maximumLatitude, maximumLongitude);
     }
 }

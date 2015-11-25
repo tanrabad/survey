5709cede68fe9a4958bde2f07687776b5a012ef2
icesuxx <ladawan_k@hotmail.co.th>
2015-11-25 15:24:37 +0700

    สร้าง class DistanceCalculateEllipsoid เพื่อคำนวณระยะทางในทรงกลมรีแบบโลก

--- domain/src/main/java/th/or/nectec/tanrabad/domain/DistanceCalculateEllipsoid.java
+++ domain/src/main/java/th/or/nectec/tanrabad/domain/DistanceCalculateEllipsoid.java
@@ -1,7 +1,80 @@
 package th.or.nectec.tanrabad.domain;
 
-/**
- * Created by User on 25/11/2558.
- */
-public class DistanceCalculateEllipsoid {
+import th.or.nectec.tanrabad.entity.Location;
+
+public class DistanceCalculateEllipsoid implements DistanceCalculator {
+    private double a = 6378.137, b = 6356.752314, f = 0.003039;
+
+    @Override
+    public double calculate(Location currentLocation, Location targetLocation) {
+        double lat1 = currentLocation.getLatitude();
+        double lon1 = currentLocation.getLongitude();
+        double lat2 = targetLocation.getLatitude();
+        double lon2 = targetLocation.getLongitude();
+
+        double L = Math.toRadians(lon2 - lon1);
+        double U1 = Math.atan((1 - f) * Math.tan(Math.toRadians(lat1)));
+        double U2 = Math.atan((1 - f) * Math.tan(Math.toRadians(lat2)));
+        double sinU1 = Math.sin(U1), cosU1 = Math.cos(U1);
+        double sinU2 = Math.sin(U2), cosU2 = Math.cos(U2);
+
+        double cosSqAlpha;
+        double sinSigma;
+        double cos2SigmaM;
+        double cosSigma;
+        double sigma;
+
+        double lambda = L, lambdaP, iterLimit = 100;
+
+        do {
+            double sinLambda = Math.sin(lambda), cosLambda = Math.cos(lambda);
+            sinSigma = Math.sqrt((cosU2 * sinLambda)
+                            * (cosU2 * sinLambda)
+                            + (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda)
+                            * (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda)
+            );
+            if (sinSigma == 0) {
+                return 0;
+            }
+            cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda;
+            sigma = Math.atan2(sinSigma, cosSigma);
+
+            double sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
+            cosSqAlpha = 1 - sinAlpha * sinAlpha;
+            cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha;
+
+            double C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
+            lambdaP = lambda;
+            lambda = L + (1 - C) * f * sinAlpha
+                    * (sigma + C * sinSigma
+                    * (cos2SigmaM + C * cosSigma
+                    * (-1 + 2 * cos2SigmaM * cos2SigmaM)
+            )
+            );
+
+        } while (Math.abs(lambda - lambdaP) > 1e-12 && --iterLimit > 0);
+
+        if (iterLimit == 0) {
+            return 0;
+        }
+
+        double uSq = cosSqAlpha * (a * a - b * b) / (b * b);
+
+        double A = 1 + uSq / 16384
+                * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
+        double B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
+
+        double deltaSigma =
+                B * sinSigma
+                        * (cos2SigmaM + B / 4
+                        * (cosSigma
+                        * (-1 + 2 * cos2SigmaM * cos2SigmaM) - B / 6 * cos2SigmaM
+                        * (-3 + 4 * sinSigma * sinSigma)
+                        * (-3 + 4 * cos2SigmaM * cos2SigmaM)));
+
+        double s = b * A * ((sigma - deltaSigma)/1.25);
+
+        return s;
+    }
 }
+

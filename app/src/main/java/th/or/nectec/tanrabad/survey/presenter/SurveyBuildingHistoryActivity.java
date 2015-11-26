58900830bc264e6eb303fb6e7a609824109f88f9
N. Choatravee <ch.n.17699@gmail.com>
2015-11-26 11:45:38 +0700

    ทำการ implement หน้าแสดงรายการประวัติการสำรวจให้ใช้ RecyclerView

--- app/src/main/java/th/or/nectec/tanrabad/survey/presenter/SurveyBuildingHistoryActivity.java
+++ app/src/main/java/th/or/nectec/tanrabad/survey/presenter/SurveyBuildingHistoryActivity.java
@@ -19,53 +19,41 @@ package th.or.nectec.tanrabad.survey.presenter;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.UUID;
+
+import th.or.nectec.tanrabad.domain.place.PlaceController;
+import th.or.nectec.tanrabad.domain.place.PlacePresenter;
 import th.or.nectec.tanrabad.domain.survey.SurveyBuildingHistoryController;
 import th.or.nectec.tanrabad.domain.survey.SurveyBuildingPresenter;
 import th.or.nectec.tanrabad.entity.Building;
+import th.or.nectec.tanrabad.entity.Place;
 import th.or.nectec.tanrabad.entity.Survey;
 import th.or.nectec.tanrabad.survey.R;
 import th.or.nectec.tanrabad.survey.repository.InMemorySurveyRepository;
 import th.or.nectec.tanrabad.survey.repository.StubPlaceRepository;
 import th.or.nectec.tanrabad.survey.repository.StubUserRepository;
 
-import java.util.List;
-
-public class SurveyBuildingHistoryActivity extends TanrabadActivity {
+public class SurveyBuildingHistoryActivity extends TanrabadActivity implements SurveyBuildingPresenter, PlacePresenter {
 
     public static final String SURVEY_ARG = "survey_arg";
 
     private TextView placeName;
-    private ListView surveyBuildingHistoryList;
+    private RecyclerView surveyBuildingHistoryList;
     private Button surveyMoreBuildingButton;
-    private PlaceAdapter placeAdapter;
+
     private SurveyBuildingHistoryAdapter surveyBuildingHistoryAdapter;
     private SurveyBuildingHistoryController surveyBuildingHistoryController;
-    private SurveyBuildingPresenter surveyBuildingPresenter = new SurveyBuildingPresenter() {
-        @Override
-        public void alertUserNotFound() {
-            Toast.makeText(SurveyBuildingHistoryActivity.this, R.string.user_not_found, Toast.LENGTH_LONG).show();
-        }
-
-        @Override
-        public void alertPlaceNotFound() {
-            Toast.makeText(SurveyBuildingHistoryActivity.this, R.string.place_not_found, Toast.LENGTH_LONG).show();
-        }
-
-        @Override
-        public void alertSurveyBuildingsNotFound() {
-            Toast.makeText(SurveyBuildingHistoryActivity.this, R.string.survey_building_history_not_found, Toast.LENGTH_LONG).show();
-        }
-
-        @Override
-        public void displaySurveyBuildingList(List<Survey> buildings) {
-            surveyBuildingHistoryAdapter = new SurveyBuildingHistoryAdapter(SurveyBuildingHistoryActivity.this, buildings);
-            surveyBuildingHistoryList.setAdapter(surveyBuildingHistoryAdapter);
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,18 +61,31 @@ public class SurveyBuildingHistoryActivity extends TanrabadActivity {
         setContentView(R.layout.activity_history_building_list);
 
         placeName = (TextView) findViewById(R.id.place_name);
-        surveyBuildingHistoryList = (ListView) findViewById(R.id.survey_building_history_list);
+
         surveyMoreBuildingButton = (Button) findViewById(R.id.survey_more_building_button);
 
-        surveyBuildingHistoryList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        showPlaceInfo();
+        setupBuildingHistoryList();
+        showSurveyBuildingHistoryList();
+    }
+
+    private void showPlaceInfo() {
+        PlaceController placeController = new PlaceController(new StubPlaceRepository(), this);
+        placeController.showPlace(UUID.fromString(getPlaceUuidFromIntent()));
+    }
+
+    private void setupBuildingHistoryList() {
+        surveyBuildingHistoryAdapter = new SurveyBuildingHistoryAdapter(this);
+        surveyBuildingHistoryList = (RecyclerView) findViewById(R.id.survey_building_history_list);
+        surveyBuildingHistoryList.setAdapter(surveyBuildingHistoryAdapter);
+        surveyBuildingHistoryList.setLayoutManager(new LinearLayoutManager(this));
+        surveyBuildingHistoryAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Survey survey = surveyBuildingHistoryAdapter.getItem(position);
                 openSurveyActivity(survey.getSurveyBuilding());
             }
         });
-
-        showSurveyBuildingHistoryList();
     }
 
     private void openSurveyActivity(Building building) {
@@ -94,12 +95,17 @@ public class SurveyBuildingHistoryActivity extends TanrabadActivity {
         startActivity(intent);
     }
 
+    @Override
+    public void displayPlace(Place place) {
+        placeName.setText(place.getName());
+    }
+
     private void showSurveyBuildingHistoryList() {
         surveyBuildingHistoryController = new SurveyBuildingHistoryController(new StubUserRepository(),
                 new StubPlaceRepository(),
                 InMemorySurveyRepository.getInstance(),
-                this.surveyBuildingPresenter);
-        surveyBuildingHistoryController.showSurveyBuildingOf(getUuidFromIntent(), getUserNameFromIntent());
+                this);
+        surveyBuildingHistoryController.showSurveyBuildingOf(getPlaceUuidFromIntent(), getUserNameFromIntent());
         surveyMoreBuildingButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -109,7 +115,27 @@ public class SurveyBuildingHistoryActivity extends TanrabadActivity {
         });
     }
 
-    private String getUuidFromIntent() {
+    @Override
+    public void alertUserNotFound() {
+        Toast.makeText(SurveyBuildingHistoryActivity.this, R.string.user_not_found, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void alertPlaceNotFound() {
+        Toast.makeText(SurveyBuildingHistoryActivity.this, R.string.place_not_found, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void alertSurveyBuildingsNotFound() {
+        Toast.makeText(SurveyBuildingHistoryActivity.this, R.string.survey_building_history_not_found, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void displaySurveyBuildingList(List<Survey> surveys) {
+        surveyBuildingHistoryAdapter.updateData(surveys);
+    }
+
+    private String getPlaceUuidFromIntent() {
         return getIntent().getStringExtra("placeUUID");
     }
 
@@ -119,7 +145,7 @@ public class SurveyBuildingHistoryActivity extends TanrabadActivity {
 
     private void openBuildingListActivity() {
         Intent intent = new Intent(SurveyBuildingHistoryActivity.this, BuildingListActivity.class);
-        intent.putExtra(BuildingListActivity.PLACE_UUID_ARG, getUuidFromIntent());
+        intent.putExtra(BuildingListActivity.PLACE_UUID_ARG, getPlaceUuidFromIntent());
         intent.putExtra(SurveyActivity.USERNAME_ARG, "sara");
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);

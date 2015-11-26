5aad158eec113ccd9132000b9a591ada55e1ad13
N. Choatravee <ch.n.17699@gmail.com>
2015-11-26 11:41:32 +0700

    สร้าง interface สำหรับ Adapter ของ RecyclerView เพื่อกำหนดคุณลักษณะเบื้องต้นของ Adapter

---
+++ app/src/main/java/th/or/nectec/tanrabad/survey/presenter/ListViewAdapter.java
@@ -0,0 +1,17 @@
+package th.or.nectec.tanrabad.survey.presenter;
+
+import android.widget.AdapterView;
+
+import java.util.List;
+
+public interface ListViewAdapter<T> {
+    void updateData(List<T> dataList);
+
+    void clearData();
+
+    T getItem(int position);
+
+    void setOnItemClickListener(AdapterView.OnItemClickListener onItemClickListener);
+
+    void setOnItemLongClickListener(AdapterView.OnItemLongClickListener onItemLongClickListener);
+}

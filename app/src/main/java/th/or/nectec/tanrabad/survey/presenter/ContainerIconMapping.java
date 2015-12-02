26ab17fd6196ac39e3bb1a9c4563d7dbb11710cb
Piruin Panichphol <piruin.p@gmail.com>
2015-12-02 16:26:22 +0700

    Rename icon สำหรับ container_type ทั้งหมด

--- app/src/main/java/th/or/nectec/tanrabad/survey/presenter/ContainerIconMapping.java
+++ app/src/main/java/th/or/nectec/tanrabad/survey/presenter/ContainerIconMapping.java
@@ -9,16 +9,16 @@ public class ContainerIconMapping {
     HashMap<Integer, Integer> containerIconMapper = new HashMap<>();
 
     public ContainerIconMapping() {
-        containerIconMapper.put(1, R.mipmap.jar);
-        containerIconMapper.put(2, R.mipmap.bottle24);
-        containerIconMapper.put(3, R.mipmap.glass37);
-        containerIconMapper.put(4, R.mipmap.ant_tray);
-        containerIconMapper.put(5, R.mipmap.ornament84);
-        containerIconMapper.put(6, R.mipmap.lotus);
-        containerIconMapper.put(7, R.mipmap.vehicle40);
-        containerIconMapper.put(8, R.mipmap.leaf1);
-        containerIconMapper.put(9, R.mipmap.garbages);
-        containerIconMapper.put(10, R.mipmap.bowl3);
+        containerIconMapper.put(1, R.mipmap.ic_container_jar);
+        containerIconMapper.put(2, R.mipmap.ic_container_bottle);
+        containerIconMapper.put(3, R.mipmap.ic_container_glass);
+        containerIconMapper.put(4, R.mipmap.ic_container_ant_tray);
+        containerIconMapper.put(5, R.mipmap.ic_container_ornament);
+        containerIconMapper.put(6, R.mipmap.ic_container_lotus);
+        containerIconMapper.put(7, R.mipmap.ic_container_vehicle);
+        containerIconMapper.put(8, R.mipmap.ic_container_leaf);
+        containerIconMapper.put(9, R.mipmap.ic_container_garbages);
+        containerIconMapper.put(10, R.mipmap.ic_container_bowl);
     }
 
     public int getContainerIcon(ContainerType containerType) {

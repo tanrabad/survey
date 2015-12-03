eba15c7f11f1e2bdb37c814ce19233e894ad6bb4
Puy <porntipa.chok@gmail.com>
2015-12-03 16:10:36 +0700

    ui Test รายชื่ออาคารของหมู่บ้าน  #acceptance

--- app/src/androidTest/java/th/or/nectec/tanrabad/survey/Suite/AcceptanceTestSuite.java
+++ app/src/androidTest/java/th/or/nectec/tanrabad/survey/Suite/AcceptanceTestSuite.java
@@ -19,13 +19,15 @@ package th.or.nectec.tanrabad.survey.Suite;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import th.or.nectec.tanrabad.survey.presenter.acceptance.BuildingTypeVillageCommunityListSurveyTest;
 import th.or.nectec.tanrabad.survey.presenter.acceptance.MainStartSurveyTest;
 import th.or.nectec.tanrabad.survey.presenter.acceptance.PlaceListInDatabaseTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         MainStartSurveyTest.class,
-        PlaceListInDatabaseTest.class
+        PlaceListInDatabaseTest.class,
+        BuildingTypeVillageCommunityListSurveyTest.class
 })
 
 public class AcceptanceTestSuite {

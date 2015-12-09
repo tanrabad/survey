895fabd6ff4a8d704c29cc856d0957c13f0d1794
Puy <porntipa.chok@gmail.com>
2015-12-09 16:01:20 +0700

    Refactor SurveyBuildingTypeVillageCommunityTest

--- app/src/androidTest/java/th/or/nectec/tanrabad/survey/presenter/SurveyBuildingTypeVillageCommunityTest.java
+++ app/src/androidTest/java/th/or/nectec/tanrabad/survey/presenter/SurveyBuildingTypeVillageCommunityTest.java
@@ -18,24 +18,29 @@
 package th.or.nectec.tanrabad.survey.presenter;
 
 import android.content.Intent;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
+
+import android.support.test.runner.AndroidJUnit4;
 import org.junit.Before;
 import org.junit.Test;
+
+import org.junit.runner.RunWith;
 import th.or.nectec.tanrabad.survey.R;
 import th.or.nectec.tanrabad.survey.TanrabadEspressoTestBase;
-import th.or.nectec.tanrabad.survey.presenter.SurveyActivity;
 
 import java.util.UUID;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+@RunWith(AndroidJUnit4.class)
 public class SurveyBuildingTypeVillageCommunityTest extends TanrabadEspressoTestBase {
+
     public ActivityTestRule<SurveyActivity> mActivityTestRule
             = new ActivityTestRule<>(SurveyActivity.class);
     SurveyActivity mActivity;
@@ -50,23 +55,28 @@ public class SurveyBuildingTypeVillageCommunityTest extends TanrabadEspressoTest
 
     @Test
     public void openSurveyHouseShouldFoundHouseData() {
-        textDisplayed(R.string.title_activity_survey);
-        onView(withId(R.id.torch))
-                .check(matches(isDisplayed()));
-        onView(withId(R.id.save))
-                .check(matches(isDisplayed()));
         onView(withId(R.id.building_name))
-                .check(matches(withText("บ้านเลขที่ 214/43")));
+                .check(matches(containText("214/43")));
         onView(withId(R.id.place_name))
                 .check(matches(withText("หมู่บ้านพาลาซเซตโต้")));
-        textDisplayed(R.string.number_of_resident);
-        textDisplayed(R.string.person);
-        textDisplayed(R.string.indoor);
-        textDisplayed(R.string.outdoor);
+    }
+
+    @Test
+    public void touchSaveByNotTypeResidentThenTouchGotItShouldStayOn() {
         onView(withId(R.id.save))
                 .perform(click());
         textDisplayed(R.string.please_enter_resident);
         onView(withText(R.string.got_it))
+                .perform(click());
+    }
+
+    @Test
+    public void touchPressBackThenTouchNoShouldStayOn() {
+        pressBack();
+        onView(containText("214/43"))
+                .check(matches(isDisplayed()));
+        textDisplayed(R.string.yes);
+        onView(withText(R.string.no))
                 .perform(click());
     }
 }

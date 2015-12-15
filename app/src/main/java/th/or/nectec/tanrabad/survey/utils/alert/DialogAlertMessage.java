5ea020f3336b6a19bfb318c4a22dfaa40a6e3702
Piruin Panichphol <piruin.p@gmail.com>
2015-12-16 01:45:05 +0700

    เพิ่ม icon สำหรับ alertDialog ประเถทแจ้งร้ายแรง

--- app/src/main/java/th/or/nectec/tanrabad/survey/utils/alert/DialogAlertMessage.java
+++ app/src/main/java/th/or/nectec/tanrabad/survey/utils/alert/DialogAlertMessage.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright (c) 2015 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package th.or.nectec.tanrabad.survey.utils.alert;
 
 import android.content.Context;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
 import android.widget.TextView;
-
 import th.or.nectec.tanrabad.survey.R;
 
 class DialogAlertMessage implements AlertMessage {
@@ -17,7 +33,7 @@ class DialogAlertMessage implements AlertMessage {
     @Override
     public void show(String message) {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
-        alertDialog.setView(R.layout.dialog_message_layout);
+        alertDialog.setView(R.layout.dialog_message_alert);
         alertDialog.setPositiveButton(R.string.got_it, null);
         AlertDialog dialog = alertDialog.show();
         TextView messageView = (TextView) dialog.findViewById(R.id.dialog_message);

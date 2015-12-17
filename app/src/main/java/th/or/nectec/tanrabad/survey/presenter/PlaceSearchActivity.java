00de77d3c95b9a18cd56df533c2fba023f9fa40a
N. Choatravee <ch.n.17699@gmail.com>
2015-12-17 13:51:20 +0700

    เพิ่ม searchview ลงใน actionbar ของหน้าค้นหาสถานที่

--- app/src/main/java/th/or/nectec/tanrabad/survey/presenter/PlaceSearchActivity.java
+++ app/src/main/java/th/or/nectec/tanrabad/survey/presenter/PlaceSearchActivity.java
@@ -1,8 +1,13 @@
 package th.or.nectec.tanrabad.survey.presenter;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import th.or.nectec.tanrabad.survey.R;
 
@@ -17,6 +22,31 @@ public class PlaceSearchActivity extends TanrabadActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_search);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_search, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconified(false);
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }

19724d9c987338b9b06b3487775e75224a4c79ec
N. Choatravee <ch.n.17699@gmail.com>
2016-01-13 17:05:22 +0700

    สร้างตัวแปลง json สำหรับ entity ของ ContainerType และสร้างขุด test

---
+++ app/src/test/java/th/or/nectec/tanrabad/survey/presenter/job/service/jsonentity/JsonContainerTypeTest.java
@@ -0,0 +1,27 @@
+package th.or.nectec.tanrabad.survey.presenter.job.service.jsonentity;
+
+import com.bluelinelabs.logansquare.LoganSquare;
+import org.junit.Test;
+import th.or.nectec.tanrabad.entity.ContainerType;
+
+import static org.junit.Assert.assertEquals;
+
+public class JsonContainerTypeTest {
+
+    String containerTypeJson = "{\"container_type_id\":1,\"container_type_name\":\"น้ำใช้\"}";
+
+    @Test
+    public void testParseToJsonString() throws Exception {
+        JsonContainerType jsonContainerType = LoganSquare.parse(containerTypeJson, JsonContainerType.class);
+        assertEquals(1, jsonContainerType.containerTypeID);
+        assertEquals("น้ำใช้", jsonContainerType.containerTypeName);
+    }
+
+    @Test
+    public void testParseToProvinceEntity() throws Exception {
+        JsonContainerType jsonContainerType = LoganSquare.parse(containerTypeJson, JsonContainerType.class);
+        ContainerType containerType = jsonContainerType.getEntity();
+        assertEquals(1, containerType.getId());
+        assertEquals("น้ำใช้", containerType.getName());
+    }
+}

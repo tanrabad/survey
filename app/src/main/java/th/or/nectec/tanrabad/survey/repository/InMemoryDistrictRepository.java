a40c8d8823b8963d5a8bff0c9bd0b0c1aa4f3bd5
N. Choatravee <ch.n.17699@gmail.com>
2016-01-15 15:34:59 +0700

    ทำการ implement InMemoryDistrictRepository

---
+++ app/src/main/java/th/or/nectec/tanrabad/survey/repository/InMemoryDistrictRepository.java
@@ -0,0 +1,67 @@
+package th.or.nectec.tanrabad.survey.repository;
+
+import th.or.nectec.tanrabad.domain.address.DistrictRepository;
+import th.or.nectec.tanrabad.domain.address.DistrictRepositoryException;
+import th.or.nectec.tanrabad.entity.District;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryDistrictRepository implements DistrictRepository {
+
+    private static InMemoryDistrictRepository instance;
+    ArrayList<District> districts = new ArrayList<>();
+
+    public static InMemoryDistrictRepository getInstance() {
+        if (instance == null) {
+            instance = new InMemoryDistrictRepository();
+        }
+        return instance;
+    }
+
+    @Override
+    public List<District> findByProvinceCode(String provinceCode) {
+        ArrayList<District> subdistrictsInsideDistrict = new ArrayList<>();
+        for (District eachDistrict : districts) {
+            if (eachDistrict.getProvinceCode().equals(provinceCode))
+                subdistrictsInsideDistrict.add(eachDistrict);
+        }
+        return subdistrictsInsideDistrict;
+    }
+
+    @Override
+    public District findByCode(String districtCode) {
+        for (District eachDistrict : districts) {
+            if (eachDistrict.getCode().equals(districtCode))
+                return eachDistrict;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean save(District district) {
+        if (districts.contains(district))
+            throw new DistrictRepositoryException();
+        districts.add(district);
+        return true;
+    }
+
+    @Override
+    public boolean update(District district) {
+        if (!districts.contains(district))
+            throw new DistrictRepositoryException();
+        districts.set(districts.indexOf(district), district);
+        return true;
+    }
+
+    @Override
+    public void updateOrInsert(List<District> districts) {
+        for (District district : districts) {
+            try {
+                update(district);
+            } catch (DistrictRepositoryException pre) {
+                save(district);
+            }
+        }
+    }
+}

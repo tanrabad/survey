2a4c09aec445bc5f8edd810cdbfc49b0029caac4
N. Choatravee <ch.n.17699@gmail.com>
2016-01-15 14:21:30 +0700

    ทำการ implement InMemorySubdistrictRepository

---
+++ app/src/main/java/th/or/nectec/tanrabad/survey/repository/InMemorySubdistrictRepository.java
@@ -0,0 +1,67 @@
+package th.or.nectec.tanrabad.survey.repository;
+
+import th.or.nectec.tanrabad.domain.address.SubdistrictRepository;
+import th.or.nectec.tanrabad.domain.address.SubdistrictRepositoryException;
+import th.or.nectec.tanrabad.entity.Subdistrict;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemorySubdistrictRepository implements SubdistrictRepository {
+
+    private static InMemorySubdistrictRepository instance;
+    ArrayList<Subdistrict> subdistricts = new ArrayList<>();
+
+    public static InMemorySubdistrictRepository getInstance() {
+        if (instance == null) {
+            instance = new InMemorySubdistrictRepository();
+        }
+        return instance;
+    }
+
+    @Override
+    public List<Subdistrict> findByDistrictCode(String districtID) {
+        ArrayList<Subdistrict> subdistrictsInsideDistrict = new ArrayList<>();
+        for (Subdistrict eachSubdistrict : subdistricts) {
+            if (eachSubdistrict.getDistrictCode().equals(districtID))
+                subdistrictsInsideDistrict.add(eachSubdistrict);
+        }
+        return subdistrictsInsideDistrict;
+    }
+
+    @Override
+    public Subdistrict findById(String subdistrictID) {
+        for (Subdistrict eachSubdistrict : subdistricts) {
+            if (eachSubdistrict.getCode().equals(subdistrictID))
+                return eachSubdistrict;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean save(Subdistrict subdistrict) {
+        if (subdistricts.contains(subdistrict))
+            throw new SubdistrictRepositoryException();
+        subdistricts.add(subdistrict);
+        return true;
+    }
+
+    @Override
+    public boolean update(Subdistrict subdistrict) {
+        if (!subdistricts.contains(subdistrict))
+            throw new SubdistrictRepositoryException();
+        subdistricts.set(subdistricts.indexOf(subdistrict), subdistrict);
+        return true;
+    }
+
+    @Override
+    public void updateOrInsert(List<Subdistrict> subdistricts) {
+        for (Subdistrict subdistrict : subdistricts) {
+            try {
+                update(subdistrict);
+            } catch (SubdistrictRepositoryException pre) {
+                save(subdistrict);
+            }
+        }
+    }
+}
---
+++ domain/src/main/java/th/or/nectec/tanrabad/domain/address/SubdistrictRepositoryException.java
@@ -0,0 +1,4 @@
+package th.or.nectec.tanrabad.domain.address;
+
+public class SubdistrictRepositoryException extends RuntimeException {
+}

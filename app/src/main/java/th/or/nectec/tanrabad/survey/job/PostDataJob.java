84c63250cc840c0de4dac287a899a072ae5ffc1c
N. Choatravee <ch.n.17699@gmail.com>
2016-01-26 15:43:53 +0700

    สร้าง PostDataJob สำหรับการ Post ข้อมุลขึ้น Server

---
+++ app/src/main/java/th/or/nectec/tanrabad/survey/job/PostDataJob.java
@@ -0,0 +1,31 @@
+package th.or.nectec.tanrabad.survey.job;
+
+import th.or.nectec.tanrabad.survey.repository.ChangedRepository;
+import th.or.nectec.tanrabad.survey.service.UploadRestService;
+
+import java.util.List;
+
+public class PostDataJob<T> implements Job {
+
+    private ChangedRepository<T> changedRepository;
+    private UploadRestService<T> uploadRestService;
+
+    public PostDataJob(ChangedRepository<T> changedRepository, UploadRestService<T> uploadRestService) {
+        this.changedRepository = changedRepository;
+        this.uploadRestService = uploadRestService;
+    }
+
+    @Override
+    public int id() {
+        return 90000;
+    }
+
+    @Override
+    public void execute() throws JobException {
+        List<T> addList = changedRepository.getAdd();
+        for (T eachData : addList) {
+            if (uploadRestService.postData(eachData))
+                changedRepository.markUnchanged(eachData);
+        }
+    }
+}

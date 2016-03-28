7bdb433dddc37025a066c4d69521612c7b619cc2
Piruin Panichphol <piruin.p@gmail.com>
2016-03-28 14:37:56 +0700

    Add splash screen

---
+++ app/src/main/java/th/or/nectec/tanrabad/survey/presenter/SplashScreenActivity.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2016 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package th.or.nectec.tanrabad.survey.presenter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+import th.or.nectec.tanrabad.survey.R;
+
+public class SplashScreenActivity extends TanrabadActivity {
+
+    private static final int DELAY_MILLS = 4000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        openLoginActivityAfterDelayed();
+    }
+
+    private void openLoginActivityAfterDelayed() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        }, DELAY_MILLS);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!"android.intent.action.MAIN".equals(getIntent().getAction())) {
+            super.onBackPressed();
+        }
+    }
+}

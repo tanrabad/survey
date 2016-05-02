589cf1109bbba98d6d5418fbf9fe42f96226316d
Piruin Panichphol <piruin.p@gmail.com>
2016-05-02 23:57:18 +0700

    Split catch exception block

--- app/src/main/java/org/tanrabad/survey/utils/PopupMenuUtil.java
+++ app/src/main/java/org/tanrabad/survey/utils/PopupMenuUtil.java
@@ -1,13 +1,28 @@
+/*
+ * Copyright (c) 2016 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.tanrabad.survey.utils;
 
 import android.view.Menu;
+import org.tanrabad.survey.TanrabadApp;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-/**
- * Created by chncs23 on 19/4/2559.
- */
 public class PopupMenuUtil {
     public static void showPopupMenuIcon(Menu menu) {
         if (menu != null) {
@@ -17,8 +32,12 @@ public class PopupMenuUtil {
                             "setOptionalIconsVisible", Boolean.TYPE);
                     m.setAccessible(true);
                     m.invoke(menu, true);
-                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                    e.printStackTrace();
+                } catch (NoSuchMethodException e) {
+                    TanrabadApp.log(e);
+                } catch (InvocationTargetException e) {
+                    TanrabadApp.log(e);
+                } catch (IllegalAccessException e) {
+                    TanrabadApp.log(e);
                 }
             }
         }

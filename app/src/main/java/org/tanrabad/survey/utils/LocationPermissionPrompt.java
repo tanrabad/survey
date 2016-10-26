3c90181d6373786161a1087204d96edc7db5246e
Piruin Panichphol <piruin.p@gmail.com>
2016-10-26 13:43:14 +0700

    update Location-Permission request error on Android 6.0

--- app/src/main/java/org/tanrabad/survey/utils/LocationPermissionPrompt.java
+++ app/src/main/java/org/tanrabad/survey/utils/LocationPermissionPrompt.java
@@ -2,7 +2,10 @@ package org.tanrabad.survey.utils;
 
 import android.Manifest;
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.support.v4.app.ActivityCompat;
+
 import org.tanrabad.survey.utils.prompt.AlertDialogPromptMessage;
 import org.tanrabad.survey.utils.prompt.PromptMessage;
 import org.tanrabad.survey.R;
@@ -10,34 +13,38 @@ import org.tanrabad.survey.R;
 public class LocationPermissionPrompt {
 
     private static final int REQUEST_CODE_ASK_PERMISSIONS = 777;
+    public static final String PERMISSION_TO_REQUEST = Manifest.permission.ACCESS_FINE_LOCATION;
 
     public static void show(final Activity activity) {
-        if (!ActivityCompat.shouldShowRequestPermissionRationale(activity,
-                Manifest.permission_group.LOCATION)) {
-
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, PERMISSION_TO_REQUEST)) {
             PromptMessage promptMessage = new AlertDialogPromptMessage(activity);
-            promptMessage.setOnConfirm(activity.getResources().getString(R.string.ok),
+            promptMessage.setOnConfirm(getString(activity, R.string.ok),
                     new PromptMessage.OnConfirmListener() {
-                        @Override
-                        public void onConfirm() {
-                            ActivityCompat.requestPermissions(activity,
-                                    new String[]{Manifest.permission_group.LOCATION},
-                                    REQUEST_CODE_ASK_PERMISSIONS);
-                        }
-                    });
-            promptMessage.setOnCancel(activity.getResources().getString(R.string.cancel),
+                @Override
+                public void onConfirm() {
+                    ActivityCompat.requestPermissions(activity,
+                            new String[]{PERMISSION_TO_REQUEST},
+                            REQUEST_CODE_ASK_PERMISSIONS);
+                }
+            });
+            promptMessage.setOnCancel(getString(activity, R.string.cancel),
                     new PromptMessage.OnCancelListener() {
-                        @Override
-                        public void onCancel() {
-                            activity.finish();
-                        }
-                    });
+                @Override
+                public void onCancel() {
+                    activity.finish();
+                }
+            });
             promptMessage.show(null, "คุณจำเป็นต้องเปิดใช้สิทธิ์การเข้าถึงตำแหน่งปัจจุบัน");
             return;
         }
         ActivityCompat.requestPermissions(activity,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                new String[]{PERMISSION_TO_REQUEST},
                 REQUEST_CODE_ASK_PERMISSIONS);
 
+    }
+
+    @NonNull
+    private static String getString(Activity activity, @StringRes int stringId) {
+        return activity.getResources().getString(stringId);
     }
 }

3e35e6198e0d24818bc587756465330df4052987
Piruin Panichphol <piruin.p@gmail.com>
2017-03-10 11:15:22 +0700

    update fabric version

--- app/src/main/java/org/tanrabad/survey/utils/MacAddressUtils.java
+++ app/src/main/java/org/tanrabad/survey/utils/MacAddressUtils.java
@@ -6,7 +6,7 @@ import android.net.wifi.WifiManager;
 
 public class MacAddressUtils {
     public static String getMacAddress(Context context) {
-        WifiManager manager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiManager manager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         WifiInfo info = manager.getConnectionInfo();
         return info.getMacAddress();
     }

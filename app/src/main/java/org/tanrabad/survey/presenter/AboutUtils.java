82026e723d12922d374dadf4fe1d8a457cf92193
Piruin Panichphol <piruin.p@gmail.com>
2018-12-28 12:49:41 +0700

    update to sdk 27 - update play-service version - use oss-licenses-plugin instead of license-fragment

--- app/src/main/java/org/tanrabad/survey/presenter/AboutUtils.java
+++ app/src/main/java/org/tanrabad/survey/presenter/AboutUtils.java
@@ -4,10 +4,12 @@ package org.tanrabad.survey.presenter;
 import android.app.Activity;
 import android.content.Intent;
 
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
+
 public class AboutUtils {
 
     public static void showLicense(Activity activity) {
-        Intent intent = new Intent(activity, LicenseActivity.class);
+        Intent intent = new Intent(activity, OssLicensesMenuActivity.class);
         activity.startActivity(intent);
     }
 }

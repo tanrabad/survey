9a7351f48d5887abaaf772bfe74f738222943736
Piruin Panichphol <piruin.p@gmail.com>
2019-05-26 00:22:14 +0700

    Improve about activity

--- app/src/main/java/org/tanrabad/survey/presenter/AboutActivity.java
+++ app/src/main/java/org/tanrabad/survey/presenter/AboutActivity.java
@@ -2,9 +2,9 @@ package org.tanrabad.survey.presenter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.View.OnClickListener;
 import org.tanrabad.survey.R;
@@ -16,11 +16,13 @@ public class AboutActivity extends TanrabadActivity {
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.about_contributor:
+                    openWeb(Uri.parse("https://github.com/tanrabad/survey/graphs/contributors"));
                     break;
                 case R.id.about_license:
                     AboutUtils.showLicense(AboutActivity.this);
                     break;
-                case R.id.about_eula:
+                case R.id.about_privacy:
+                    openWeb(Uri.parse("https://github.com/tanrabad/survey/blob/master/PRIVACY.md"));
                     break;
             }
         }
@@ -31,13 +33,19 @@ public class AboutActivity extends TanrabadActivity {
         activity.startActivity(intent);
     }
 
+
+    private void openWeb(Uri uri) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        setSupportActionBar(findViewById(R.id.toolbar));
         findViewById(R.id.about_contributor).setOnClickListener(onClickListener);
         findViewById(R.id.about_license).setOnClickListener(onClickListener);
-        findViewById(R.id.about_eula).setOnClickListener(onClickListener);
+        findViewById(R.id.about_privacy).setOnClickListener(onClickListener);
     }
 }

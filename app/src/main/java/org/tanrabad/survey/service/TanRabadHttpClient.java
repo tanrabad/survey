45b5acff6962064a5072cb5088988b39f13cbfe5
Piruin Panichphol <piruin.p@gmail.com>
2019-06-02 18:12:55 +0700

    add HttpLoggingInterceptor

--- app/src/main/java/org/tanrabad/survey/service/TanRabadHttpClient.java
+++ app/src/main/java/org/tanrabad/survey/service/TanRabadHttpClient.java
@@ -1,5 +1,6 @@
 package org.tanrabad.survey.service;
 
+import org.tanrabad.survey.BuildConfig;
 import org.tanrabad.survey.R;
 import org.tanrabad.survey.TanrabadApp;
 import org.tanrabad.survey.utils.http.FileCertificateAuthority;
@@ -9,6 +10,7 @@ import java.io.InputStream;
 import javax.net.ssl.X509TrustManager;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -22,13 +24,17 @@ public class TanRabadHttpClient {
             InputStream certificate = TanrabadApp.getInstance()
                 .getResources().openRawResource(R.raw.tanrabad);
             FileCertificateAuthority ca = new FileCertificateAuthority(certificate);
-            return new OkHttpClient.Builder()
+            OkHttpClient.Builder builder = new OkHttpClient.Builder()
                 .readTimeout(READ_WRITE_TIMEOUT, SECONDS)
                 .writeTimeout(READ_WRITE_TIMEOUT, SECONDS)
                 .connectTimeout(CONNECT_TIMEOUT, SECONDS)
                 .sslSocketFactory(ca.getSslContext().getSocketFactory(),
-                    (X509TrustManager) ca.getTrustManagers()[0])
-                .build();
+                    (X509TrustManager) ca.getTrustManagers()[0]);
+            if (BuildConfig.DEBUG) {
+                builder.addInterceptor(new HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY));
+            }
+            return builder.build();
         } catch (NullPointerException ignoreOnUnitTest) {
             return new OkHttpClient();
         }
